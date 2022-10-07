65bdec9eeac5b9d1b27ad3285eae0b6cce1d9e63
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-10-07 15:48:07 -0300

    adding react project in repository

---
+++ healthinventory-react/src/App.js
@@ -0,0 +1,14 @@
+import '../src/styles/main.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-full flex flex-" >
+        <div className="">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
