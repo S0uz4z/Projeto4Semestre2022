6688e21757f5e197dcb1f9a69a427050565765d9
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-10 12:38:16 -0300

    adicionando rotas

--- healthinventory-react/src/App.tsx
+++ healthinventory-react/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../src/styles/main.css'
 import Login from './pages/Login';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AdminPage from './pages/AdminPage';
+import ManutencaoInventario from './pages/ManutencaoInventario';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login></Login>}></Route>
+        <Route path='/index' element={<AdminPage></AdminPage>}>
+          <Route path='manutencaoInventario' element={<ManutencaoInventario items={[{id:1, nome:'Lápis', quantidade: 5}]}></ManutencaoInventario>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
