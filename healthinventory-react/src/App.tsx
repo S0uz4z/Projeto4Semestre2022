f90d78b328e21111ce660568d703e2f6d97b8342
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-10 02:18:32 -0300

    adicionando componente de login ao app.tsx

--- healthinventory-react/src/App.tsx
+++ healthinventory-react/src/App.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import '../src/styles/main.css'
-import Button from '../src/components/Button'
-import InputText from '../src/components/InputText'
+import Login from './pages/Login';
+
 
 function App() {
   return (
-    <div className="App flex">
-      <div className="w-[40%] h-screen flex flex-col bg-[#10543c]">
-        <div className="w-[40%] m-auto text-white text-center flex flex-col">
-          <i className="fa-solid fa-clipboard text-9xl py-3"></i>
-          <span>HEALTHINVENTORY</span>
-        </div>
-      </div>
-      <div className="w-[60%] bg-white flex">
-        <div className="m-auto text-center">
-          <span className="font-extrabold text-5xl text-[#10543c] block mb-12">Login</span>
-          <InputText placeholder='Usuário'></InputText>
-          <InputText placeholder='Senha' password={true}></InputText>
-          <Button text='Entrar'></Button>
-          <a href='#'>
-            <span className="font-medium text-[#10543c] underline">Esqueceu sua senha?</span>
-          </a>
-        </div>
-      </div>
-    </div>
+    <Login></Login>
   );
 }
 
---
+++ healthinventory-react/src/pages/Login.tsx
@@ -0,0 +1,26 @@
+import Button from '../components/Button'
+import InputText from '../components/InputText'
+
+export default function Login() {
+    return (
+        <div className="App flex">
+            <div className="w-[40%] h-screen flex flex-col bg-[#10543c]">
+                <div className="w-[40%] m-auto text-white text-center flex flex-col">
+                    <i className="fa-solid fa-clipboard text-9xl py-3"></i>
+                    <span>HEALTHINVENTORY</span>
+                </div>
+            </div>
+            <div className="w-[60%] bg-white flex">
+                <div className="m-auto text-center">
+                    <span className="font-extrabold text-5xl text-[#10543c] block mb-12">Login</span>
+                    <InputText placeholder='Usuário'></InputText>
+                    <InputText placeholder='Senha' password={true}></InputText>
+                    <Button text='Entrar'></Button>
+                    <a href='#'>
+                        <span className="font-medium text-[#10543c] underline">Esqueceu sua senha?</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
