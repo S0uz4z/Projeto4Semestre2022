6174545ecdd280eadce9291469febf481ddf75a5
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-10 12:39:04 -0300

    alterando margem padrao do botao

--- healthinventory-react/src/components/Button.tsx
+++ healthinventory-react/src/components/Button.tsx
@@ -6,8 +6,8 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
     return (
-        <div className="m-8">
-            <button className="bg-[#10543c] px-10 py-3 text-white font-semibold text-2xl">{props.text}</button>
+        <div className="m-5">
+            <button type='button' className="bg-[#10543c] px-10 py-3 text-white font-semibold text-2xl">{props.text}</button>
         </div>
     )
 }
