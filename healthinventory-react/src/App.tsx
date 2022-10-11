44bb2f22a0adabdd41f13f4d2428e23d66db1a29
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-11 00:59:00 -0300

    removendo rotas não usadas

--- healthinventory-react/src/App.tsx
+++ healthinventory-react/src/App.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import '../src/styles/main.css'
 import Login from './pages/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AdminPage from './pages/AdminPage';
-import ManutencaoInventario from './pages/ManutencaoInventario';
+import Menu from './pages/Menu';
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login></Login>}></Route>
-        <Route path='/index' element={<AdminPage></AdminPage>}>
-          <Route path='manutencaoInventario' element={<ManutencaoInventario items={[{id:1, nome:'Lápis', quantidade: 5}]}></ManutencaoInventario>}></Route>
-        </Route>
+        <Route path='/index' element={<Menu></Menu>}></Route>
       </Routes>
     </BrowserRouter>
   )
