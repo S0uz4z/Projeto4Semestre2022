f0a008b20eed78fb7ef70d75b543328c1dc7496b
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-11 00:58:38 -0300

    organizando paginas, criando paginda de menu

---
+++ healthinventory-react/src/pages/Menu.tsx
@@ -0,0 +1,36 @@
+import Navbar from "../components/Navbar";
+import AdminContainer from "./AdminContainer";
+
+const usuario = {
+    id: 1,
+    nome: "João Silva",
+    tipo: { valor: "admin", label: "Administrador" }
+}
+
+interface Usuario {
+    id: number
+    nome: string
+    tipo: Tipo
+}
+
+interface Tipo {
+    valor: string
+    label: string
+}
+
+const renderMenu = (usuario: Usuario) => {
+    if (usuario.tipo.valor == "admin") {
+        return (<AdminContainer></AdminContainer>)
+    } else {
+        // return(<UsuarioContainer></UsuarioContainer>)
+    }
+}
+
+export default function Menu() {
+    return (
+        <div>
+            <Navbar text={`Olá ${usuario.tipo.label}!`}></Navbar>
+            {renderMenu(usuario)}
+        </div>
+    )
+}
