3360c63adc8a9af4b716191e01d6a20757a6443a
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-22 15:58:42 -0300

    criando tabela

---
+++ healthinventory-react/src/pages/Solicitacoes.tsx
@@ -0,0 +1,11 @@
+export default function Solicitacoes() {
+    return (
+        <div className="w-full h-screen mt-10">
+            <div className="w-[50%] min-h-[50%] border-black border-2 border-solid m-auto">
+                <div className="block text-center mt-3 ">
+                    <span className="font-bold text-4xl text-black border-b-[#10543c] border-b-4 border-solid">Solicitações</span>
+                </div>
+            </div>
+        </div>
+    )
+}
