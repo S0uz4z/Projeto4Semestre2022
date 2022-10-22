169062b7d7721f48101a90506b82a24e0fe71bd0
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-22 16:45:23 -0300

    criando funcao login

--- healthinventory-react/src/pages/Login.tsx
+++ healthinventory-react/src/pages/Login.tsx
@@ -1,9 +1,65 @@
 import Button from '../components/Button'
 import InputText from '../components/InputText'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-function entrar(){console.log('asdased')}
+
+function entrar() {
+    let usuario = document.getElementById('usuario') as HTMLInputElement
+    let senha = document.getElementById('senha') as HTMLInputElement
+    
+    var data = JSON.stringify({
+        "usuario": usuario.value,
+        "senha": senha.value
+    });
+
+    var config = {
+        method: 'post',
+        url: 'http://localhost:7000/login',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    function entrar() {
+        let usuario = document.getElementById('usuario') as HTMLInputElement
+        let senha = document.getElementById('senha') as HTMLInputElement
+
+        var data = JSON.stringify({
+            "usuario": usuario.value,
+            "senha": senha.value
+        });
+
+        var config = {
+            method: 'post',
+            url: 'http://localhost:7000/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                navigate('/index')
+            })
+            .catch(function (error) {
+                console.log(error.response.data.message);
+            });
+    }
     return (
         <div className="App flex">
             <div className="w-[40%] h-screen flex flex-col bg-[#10543c]">
@@ -15,8 +71,8 @@ export default function Login() {
             <div className="w-[60%] bg-white flex">
                 <div className="m-auto text-center">
                     <span className="font-extrabold text-5xl text-[#10543c] block mb-12">Login</span>
-                    <InputText placeholder='Usuário'></InputText>
-                    <InputText placeholder='Senha' password={true}></InputText>
+                    <InputText id='usuario' placeholder='Usuário'></InputText>
+                    <InputText id='senha' placeholder='Senha' password={true}></InputText>
                     <div className='m-8'>
                         <Button function={entrar} text='Entrar'></Button>
                     </div>
