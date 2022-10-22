a6ea64703e9e29322ab9860b53b8dff22185771c
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-22 16:45:41 -0300

    adicionando propriedade id ao componente input text

--- healthinventory-react/src/components/InputText.tsx
+++ healthinventory-react/src/components/InputText.tsx
@@ -1,12 +1,13 @@
 interface InputTextProps {
     placeholder: string,
     password?: boolean,
+    id?: string
 }
 
 export default function InputText(props: InputTextProps) {
     return (
         <div className="m-5">
-            <input type={props.password ? "password" : "text"} placeholder={props.placeholder} className="border-spacing-0 p-3 text-base bg-[#d9d9d9] text-[#8c8c8c]
+            <input id={props.id} type={props.password ? "password" : "text"} placeholder={props.placeholder} className="border-spacing-0 p-3 text-base bg-[#d9d9d9] text-[#8c8c8c]
             focus: outline-none" />
         </div>
     )
